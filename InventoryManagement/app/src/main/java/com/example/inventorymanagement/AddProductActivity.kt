a23ff981a74122d5714cc90c31cae738c44cfdd1
Anupam-dagar <dagaranupam@gmail.com>
2020-07-23 15:21:47 +0530

    add AddProductActivity and set its content view.

---
+++ InventoryManagement/app/src/main/java/com/example/inventorymanagement/AddProductActivity.kt
@@ -0,0 +1,13 @@
+package com.example.inventorymanagement
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AddProductActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.add_product)
+    }
+
+}
