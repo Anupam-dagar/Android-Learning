ff6fb124910806c0d551597300b5cf6e28d2b290
Anupam-dagar <dagaranupam@gmail.com>
2020-07-23 17:14:20 +0530

    add context and save user input for product name. This commit persists the user input for product name and saves it using shared preferences. The goal is to save and display user input.

--- InventoryManagement/app/src/main/java/com/example/inventorymanagement/AddProductActivity.kt
+++ InventoryManagement/app/src/main/java/com/example/inventorymanagement/AddProductActivity.kt
@@ -1,13 +1,25 @@
 package com.example.inventorymanagement
 
+import android.content.Context
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.add_product.*
 
 class AddProductActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.add_product)
+
+        val submitButton = findViewById<Button>(R.id.submitButton)
+        submitButton.setOnClickListener {
+            val database = getSharedPreferences("database", Context.MODE_PRIVATE)
+            database.edit().apply {
+                putString("savedProductName", editProduct.text.toString())
+            }.apply()
+        }
+
     }
 
 }
--- InventoryManagement/app/src/main/java/com/example/inventorymanagement/MainActivity.kt
+++ InventoryManagement/app/src/main/java/com/example/inventorymanagement/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.inventorymanagement
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log.d
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
@@ -18,8 +20,14 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(findViewById(R.id.toolbar))
 
         val gotoButton = findViewById<Button>(R.id.goToAddProduct)
-        gotoButton.setOnClickListener{
+        gotoButton.setOnClickListener {
             startActivity(Intent(this, AddProductActivity::class.java))
         }
+
+        val preferences = getSharedPreferences("database", Context.MODE_PRIVATE)
+        val savedName = preferences.getString("savedProductName", "This value doesn't exist")
+        d("Anupam", "Saved message is: $savedName")
+
+        lastSavedProduct.text = savedName
     }
 }
