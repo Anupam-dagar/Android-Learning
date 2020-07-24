b7a94714b738671d7c3ae7cfa68e72f44149db53
Anupam-dagar <dagaranupam@gmail.com>
2020-07-24 15:36:32 +0530

    add Product data class, use a Kotlin coroutine to make a background network call and update the textView, display a list of Product using textView on main screen

--- InventoryManagement/app/src/main/java/com/example/inventorymanagement/MainActivity.kt
+++ InventoryManagement/app/src/main/java/com/example/inventorymanagement/MainActivity.kt
@@ -10,7 +10,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Button
+import androidx.lifecycle.lifecycleScope
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,15 +24,39 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
 
-        val gotoButton = findViewById<Button>(R.id.goToAddProduct)
-        gotoButton.setOnClickListener {
+        val addProductButton = findViewById<FloatingActionButton>(R.id.fab)
+        addProductButton.setOnClickListener {
             startActivity(Intent(this, AddProductActivity::class.java))
         }
 
-        val preferences = getSharedPreferences("database", Context.MODE_PRIVATE)
-        val savedName = preferences.getString("savedProductName", "This value doesn't exist")
-        d("Anupam", "Saved message is: $savedName")
+        val products = listOf(
+            Product("Bread", "Anupam", 2006, 260.50),
+            Product("Keyboard", "John", 2007, 500.00),
+            Product("Bread", "Anupam", 2008, 12.34),
+            Product("Keyboard", "John", 2009, 50.20)
+        )
 
-        lastSavedProduct.text = savedName
+        var totalCost = 0.0
+
+        products.forEach {
+            totalCost += it.cost
+            productsTextView.append("${it.name} - ${it.owner} - ${it.yearPurchased} - $ ${it.cost}\n\n")
+        }
+
+        totalCostTextView.text = "$ ${totalCost}"
+
+//        val preferences = getSharedPreferences("database", Context.MODE_PRIVATE)
+//        val savedName = preferences.getString("savedProductName", "This value doesn't exist")
+//        d("Anupam", "Saved message is: $savedName")
+
+//        lastSavedProduct.text = savedName
+
+//        lifecycleScope.launch(Dispatchers.Default) {
+//            val specialMessage = URL("https://finepointmobile.com/api/inventory/v1/message").readText()
+//            d("Anupam", "The message is: $specialMessage")
+//
+//            lastSavedProduct.text = specialMessage
+//        }
+
     }
 }
