511efb66db63e3f242c8227ed36a4f7906a69fb7
Anupam-dagar <dagaranupam@gmail.com>
2020-07-27 13:44:33 +0530

    add custom action to receiver app, check how the activity intent was started and set dynamic text accordingly.

--- IntentFilters2Apps/ReceiverApp/app/src/main/java/com/example/receiverapp/MainActivity.kt
+++ IntentFilters2Apps/ReceiverApp/app/src/main/java/com/example/receiverapp/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.receiverapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    var displayText = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        displayText =
+            if (intent.action == "android.intent.action.MAIN") "Hello World! Send text from sender app!" else {
+                intent.getStringExtra("sender_text").toString()
+            }
+
+        val senderTextView = findViewById<TextView>(R.id.senderText)
+        senderTextView.text = displayText
     }
 }
