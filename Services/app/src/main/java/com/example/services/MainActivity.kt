b91c8754f532bf85eab1d0f60e4504be4d0fb402
Anupam-dagar <dagaranupam@gmail.com>
2020-07-29 14:02:02 +0530

    implement bindService, unBindService methods for the random number generator service. Create a connection using service connection and display the random number generated.

--- Services/app/src/main/java/com/example/services/MainActivity.kt
+++ Services/app/src/main/java/com/example/services/MainActivity.kt
@@ -1,12 +1,33 @@
 package com.example.services
 
+import android.content.ComponentName
+import android.content.Context
 import android.content.Intent
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
 import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var serviceIntent: Intent
+
+    private lateinit var randomNumberService: RandomNumberService
+    private var isRandomNumberServiceBound = false
+
+    private val serviceConnection = object : ServiceConnection {
+        override fun onServiceConnected(className: ComponentName, service: IBinder) {
+            val binder = service as RandomNumberService.RandomNumberServiceBinder
+            randomNumberService = binder.getService()
+            isRandomNumberServiceBound = true
+        }
+
+        override fun onServiceDisconnected(arg0: ComponentName) {
+            isRandomNumberServiceBound = false
+        }
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,5 +42,22 @@ class MainActivity : AppCompatActivity() {
 
     fun serviceStopper(view: View) {
         stopService(serviceIntent)
+    }
+
+    fun serviceBinder(view: View) {
+        bindService(serviceIntent, serviceConnection, Context.BIND_AUTO_CREATE)
+    }
+
+    fun serviceUnbinder(view: View) {
+        if (isRandomNumberServiceBound) {
+            unbindService(serviceConnection)
+            isRandomNumberServiceBound = false
+        }
+    }
+
+    fun showRandomNumber(view: View) {
+        val displayView = findViewById<TextView>(R.id.randomNumberTextView)
+        displayView.text = if (isRandomNumberServiceBound) randomNumberService.getRandomNumber()
+            .toString() else "Service not bound, generate a random number."
     }
 }
